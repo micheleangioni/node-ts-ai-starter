390bb504e2238d2975cb3a9e2c0e189b28e934b4
Michele Angioni <michele.angioni@takeaway.com>
2018-09-10 13:58:20 +0200

    Updated Mongoose User Model definition

--- src/infra/mongo/models/users.ts
+++ src/infra/mongo/models/users.ts
@@ -2,12 +2,12 @@ import express from 'express';
 
 export default function (app: express.Application) {
   const mongoose = app.get('mongooseClient');
-  const {Schema} = mongoose;
+  const { Schema } = mongoose;
 
   const users = new Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    username: {type: String, unique: true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, unique: true, sparse: true },
   });
 
   return mongoose.model('users', users);
