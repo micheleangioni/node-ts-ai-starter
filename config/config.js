bc319b675cf0d2f6bc4f86b8da02a5ca7fadaa07
Michele Angioni <michele.angioni@takeaway.com>
2018-10-08 14:43:44 +0200

    Added sequelize and umzug packages and configurations

---
+++ config/config.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.join(__dirname, '../.env') });
+
+console.log('ENVIRONMENT', process.env)
+
+module.exports = {
+  local: {
+    username: process.env.SQL_USERNAME,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_NAME,
+    host: process.env.SQL_HOST,
+    dialect: process.env.SQL_DIALECT
+  },
+  test: {
+    username: process.env.SQL_USERNAME,
+    password: process.env.SQL_PASSWORD,
+    database: `${process.env.SQL_NAME}-test`,
+    host: process.env.SQL_HOST,
+    dialect: 'sqlite'
+  },
+  production: {
+    username: process.env.SQL_USERNAME,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_NAME,
+    host: process.env.SQL_HOST,
+    dialect: process.env.SQL_DIALECT
+  }
+};
