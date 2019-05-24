ffc930e75c37598fbc30828bdeb5b0f3e5e9b372
Michele Angioni <michele.angioni@takeaway.com>
2019-05-24 16:09:54 +0200

    Removed build from version control

--- build/application/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(app) {
-    // Add application services
-}
-exports.default = default_1;
-//# sourceMappingURL=index.js.map
--- build/infra/sql/models/users/user.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-class User extends sequelize_1.Model {
-}
-exports.default = User;
-//# sourceMappingURL=user.js.map
