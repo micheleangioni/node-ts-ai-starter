b419e3a564f8767f195614f1f3a0101ea38b4e00
Michele Angioni <michele.angioni@takeaway.com>
2019-05-24 19:32:33 +0200

    Added ci environment to Sequelize CLI conf

--- config/config.js
+++ config/config.js
@@ -11,6 +11,13 @@ module.exports = {
     host: process.env.SQL_HOST,
     dialect: process.env.SQL_DIALECT
   },
+  ci: {
+    username: process.env.SQL_USERNAME,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_NAME,
+    host: process.env.SQL_HOST,
+    dialect: process.env.SQL_DIALECT
+  },
   test: {
     username: process.env.SQL_USERNAME,
     password: process.env.SQL_PASSWORD,
