e75c20bff186e05c5bf48394f51599d14fb883df
Michele Angioni <michele.angioni@takeaway.com>
2023-07-01 13:26:38 +0200

    Added missing type definition QueryDocsOptions

--- src/application/llm/declarations.ts
+++ src/application/llm/declarations.ts
@@ -1,8 +1,17 @@
+import {SaveableVectorStore} from 'langchain/dist/vectorstores/base';
+
 export type ChatOptions = {
   context?: string;
   maxTokens?: number;
   temperature?: number;
   template?: string;
+  verbose?: boolean;
+};
+
+export type QueryDocsOptions = {
+  maxTokens?: number;
+  temperature?: number;
+  vectorStore: SaveableVectorStore;
   verbose?: boolean;
 };
 
