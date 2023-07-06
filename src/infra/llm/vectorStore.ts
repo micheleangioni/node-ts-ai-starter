eee4e4270adbe71b1da6dfa5b50cbf23b45a3947
Michele Angioni <michele.angioni@takeaway.com>
2023-07-06 22:15:09 +0200

    Added memory vector store and fixed the (POST)/search/load-document tests

--- src/infra/llm/vectorStore.ts
+++ src/infra/llm/vectorStore.ts
@@ -1,10 +1,12 @@
-import {HNSWLib} from 'langchain/vectorstores/hnswlib';
-import {OpenAIEmbeddings} from 'langchain/embeddings/openai';
-import {SaveableVectorStore} from 'langchain/dist/vectorstores/base';
-import config from '../../config';
 import {Document} from 'langchain/document';
+import {OpenAIEmbeddings} from 'langchain/embeddings/openai';
+import {SaveableVectorStore} from 'langchain/vectorstores/base';
+import {HNSWLib} from 'langchain/vectorstores/hnswlib';
+import {MemoryVectorStore} from 'langchain/vectorstores/memory';
+import config from '../../config';
 
-let vectorStore: SaveableVectorStore;
+// The Vector store can be a memory-based one, i.e. cannot be persisted, or a persistable one
+let vectorStore: SaveableVectorStore | MemoryVectorStore;
 const vectorStoreFolder = `./${config.llm.vectorStore.hnswlib.folder}`;
 
 export const addDocumentsToVectorStore = async (docs: Document[]) => await vectorStore.addDocuments(docs);
@@ -15,9 +17,11 @@ export const loadVectorStore = async () => {
   }
 
   switch (process.env.VECTOR_STORE?.toString()) {
+    case 'memory':
+      vectorStore = await MemoryVectorStore.fromTexts(['Initialising'], [], new OpenAIEmbeddings());
+      break;
     case 'hnswlib':
     default:
-
       try {
         vectorStore = await HNSWLib.load(vectorStoreFolder, new OpenAIEmbeddings());
       } catch (e) {
@@ -31,4 +35,8 @@ export const loadVectorStore = async () => {
   return vectorStore;
 };
 
-export const persistVectorStore = async () => await vectorStore.save(vectorStoreFolder);
+export const persistVectorStore = async () => {
+  if (vectorStore instanceof SaveableVectorStore) {
+    await vectorStore.save(vectorStoreFolder);
+  }
+};
