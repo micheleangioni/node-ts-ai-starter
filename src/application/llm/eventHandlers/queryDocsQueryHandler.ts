9ac6cfeb24a254b8588d0a00327a1c9a2348ff86
Michele Angioni <michele.angioni@takeaway.com>
2023-07-06 22:43:48 +0200

    Moved the code to create an instance of RetrievalQAChain to its own infra file, added test for the (GET) /search/documents endpoint

--- src/application/llm/eventHandlers/queryDocsQueryHandler.ts
+++ src/application/llm/eventHandlers/queryDocsQueryHandler.ts
@@ -1,40 +1,30 @@
-import {ContextualCompressionRetriever} from 'langchain/retrievers/contextual_compression';
-import {OpenAI} from 'langchain/llms/openai';
-import {LLMChainExtractor} from 'langchain/retrievers/document_compressors/chain_extract';
 import {QueryDocsOptions} from '../declarations';
 import {QueryDocsQuery} from '../queries/queryDocsQuery';
 import IHandler from '../../IHandler';
 import {RetrievalQAChain} from 'langchain/chains';
+import buildRetrievalQAChain from '../../../infra/llm/buildRetrievalQAChain';
 
 export class QueryDocsQueryHandler implements IHandler {
-  private readonly chain: RetrievalQAChain;
+  private readonly maxTokens?: number;
+  private readonly temperature?: number;
+  private readonly verbose?: boolean;
+  private chain: RetrievalQAChain;
 
-  constructor({maxTokens, temperature, vectorStore, verbose}: QueryDocsOptions) {
-    const model = new OpenAI({
-      maxTokens: maxTokens?? 100,
-      modelName: 'gpt-3.5-turbo',
-      streaming: false,
-      temperature: temperature ?? 0.2,
-    });
-
-    // We want to implement Contextual Compression
-    // See https://js.langchain.com/docs/modules/indexes/retrievers/contextual-compression-retriever
-
-    // The LLMChainExtractor will iterate over the initially returned documents
-    // and extract from each only the content that is relevant to the query.
-    const baseCompressor = LLMChainExtractor.fromLLM(model);
-
-    const retriever = new ContextualCompressionRetriever({
-      baseCompressor,
-      baseRetriever: vectorStore.asRetriever(),
-    });
-
-    // We then create a Retrieval Q&A. The Retriever will be used to retrieve the docs relevant to the input query
-    // everytime the LLM will get called
-    this.chain = RetrievalQAChain.fromLLM(model, retriever, {verbose});
+  constructor({maxTokens, temperature, verbose}: QueryDocsOptions) {
+    this.maxTokens = maxTokens;
+    this.temperature = temperature;
+    this.verbose = verbose;
   }
 
   async handle({ query }: QueryDocsQuery): Promise<string> {
+    if (!this.chain) {
+      this.chain = await buildRetrievalQAChain({
+        maxTokens: this.maxTokens,
+        temperature: this.temperature,
+        verbose: this.verbose,
+      });
+    }
+
     const response = await this.chain.call({query});
 
     return response.text;
---
+++ src/infra/llm/buildRetrievalQAChain.ts
@@ -0,0 +1,36 @@
+import {OpenAI} from 'langchain/llms/openai';
+import {LLMChainExtractor} from 'langchain/retrievers/document_compressors/chain_extract';
+import {ContextualCompressionRetriever} from 'langchain/retrievers/contextual_compression';
+import {RetrievalQAChain} from 'langchain/chains';
+import {loadVectorStore} from './vectorStore';
+
+export default async ({maxTokens, temperature, verbose}: {
+  maxTokens?: number;
+  temperature?: number;
+  verbose?: boolean;
+}): Promise<RetrievalQAChain> => {
+  const vectorStore = await loadVectorStore();
+
+  const model = new OpenAI({
+    maxTokens: maxTokens?? 100,
+    modelName: 'gpt-3.5-turbo',
+    streaming: false,
+    temperature: temperature ?? 0.2,
+  });
+
+  // We want to implement Contextual Compression
+  // See https://js.langchain.com/docs/modules/indexes/retrievers/contextual-compression-retriever
+
+  // The LLMChainExtractor will iterate over the initially returned documents
+  // and extract from each only the content that is relevant to the query.
+  const baseCompressor = LLMChainExtractor.fromLLM(model);
+
+  const retriever = new ContextualCompressionRetriever({
+    baseCompressor,
+    baseRetriever: vectorStore.asRetriever(),
+  });
+
+  // We then create a Retrieval Q&A. The Retriever will be used to retrieve the docs relevant to the input query
+  // everytime the LLM will get called
+  return RetrievalQAChain.fromLLM(model, retriever, {verbose});
+};
